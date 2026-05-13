4c07b86ec133237b8287c0717e556c7dfd6e9e95
P0llo9003 <se.gomezl@duocuc.cl>
2026-05-13 12:42:58 -0400

    add cines-service

---
+++ cines-service/cines-service/src/main/resources/db/changelog/db.changelog.sql
@@ -0,0 +1,11 @@
+-- liquibase formatted sql
+
+-- changeset seba:1
+CREATE TABLE cines (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    
+);
+
+INSERT INTO usuario(run, dv, nombre, apellido, correo) VALUES
+
+();
---
+++ cines-service/cines-service/src/test/java/com/microservicio/cines_service/CinesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservicio.cines_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
