d34837e965eec4460950d3d8de29730ef843ee0e
Pollo <se.gomezl@duocuc.cl>
2026-06-21 16:21:40 -0400

    Se implementa Swagger y Docker

---
+++ build-all.bat
@@ -0,0 +1,21 @@
+rem $env:DOCKER_BUILDKIT=0
+rem docker rm -f $(docker ps -aq)
+FOR /f %%i IN ('docker ps -aq') DO docker rm -f %%i
+FOR /f %%i IN ('docker images -aq') DO docker rmi -f %%i
+cd api-gateway
+call .\mvnw clean package -DskipTests
+
+cd ../cines-service
+call .\mvnw clean package -DskipTests
+
+cd ../funciones-service
+call  .\mvnw clean package -DskipTests
+
+cd ../peliculas-service
+call .\mvnw clean package -DskipTests
+
+cd ../salas-service
+call .\mvnw clean package -DskipTests
+
+cd ../usuarios-service
+call .\mvnw clean package -DskipTests
---
+++ clean-all.bat
@@ -0,0 +1,17 @@
+cd api-gateway
+call .\mvnw clean
+
+cd ../cines-service
+call .\mvnw clean
+
+cd ../funciones-service
+call  .\mvnw clean
+
+cd ../peliculas-service
+call .\mvnw clean
+
+cd ../salas-service
+call .\mvnw clean
+
+cd ../usuarios-service
+call .\mvnw clean
